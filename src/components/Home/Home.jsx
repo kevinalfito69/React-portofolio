6133859bc6b89a87765068d3e72462c67a7100e9
kevinalfito69 <kevinalfito69@gmail.com>
2022-08-17 17:56:23 +0700

    adding Home page, and animation

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import LogoTitle from '../../assets/images/kevintekno.png'
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import './index.scss'
+import Logo from './Logo/Logo'
+const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['e', 'v', 'i', 'n', ' ', 'A', 'l', 'f', 'i', 't', 'o']
+    const jobArray = [
+        'w',
+        'e',
+        'b',
+        ' ',
+        'd',
+        'e',
+        'v',
+        'e',
+        'l',
+        'o',
+        'p',
+        'e',
+        'r',
+        '.',
+    ]
+
+    useEffect(() => {
+        let timeoutId = setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+
+        return () => {
+            clearTimeout(timeoutId)
+        }
+    }, [])
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1>
+                    <span className={letterClass}>H</span>
+                    <span className={`${letterClass} _12`}>i,</span>
+                    <br /> <span className={`${letterClass} _13`}>I</span>
+                    <span className={`${letterClass} _14`}>'m</span>
+                    <img src={LogoTitle} alt="Developer" />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={nameArray}
+                        idx={15}
+                    />
+                    <br />
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={jobArray}
+                        idx={26}
+                    />
+                </h1>
+                <h2>Frontend Developer</h2>
+                <Link to="/contact" className="flat-button">
+                    CONTACT ME
+                </Link>
+            </div>
+            <Logo />
+        </div>
+    )
+}
+
+export default Home
